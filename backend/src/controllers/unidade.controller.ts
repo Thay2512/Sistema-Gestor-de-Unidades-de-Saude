1018a587c11c7fead356365cb38ade8d12d39890
Thayane Araujo de Castro <thayanearaujo45@gmail.com>
2026-07-04 12:23:09 -0300

    feat: read, update, delete. feat read: funcao findAll impplementada de forma a buscar todas as unidades de Itajubá

--- backend/src/controllers/unidade.controller.ts
+++ backend/src/controllers/unidade.controller.ts
@@ -1,38 +1,41 @@
-import { UnidadeService } from "../services/unidade.service";
-import { Request, Response } from "express";
+import { Request, Response } from "express"
+import { UnidadeService } from "../services/unidade.service" // Ajuste o caminho se necessário
 
 const unidadeService = new UnidadeService()
 
-export class UnidadeController{
+export class UnidadeController {
 
-    async create(req: Request, res: Response){
+    async create(req: Request, res: Response) {
         const dados = req.body
+        dados.cnes = Number(dados.cnes)
+        
         await unidadeService.create(dados)
         res.status(200).send()
     }
 
-    async getAll(req: Request, res: Response){
-        const unidades = await unidadeService.getAll()
+    async getAll(req: Request, res: Response) {
+        const unidades = await unidadeService.findAll()
         res.status(200).send(unidades)
     }
 
-    async getById(req: Request, res: Response){
+    async getById(req: Request, res: Response) {
         const { id } = req.params
-        const unidade = await unidadeService.getById(id)
+        const unidade = await unidadeService.findById(Number(id))
         res.status(200).send(unidade)
     }
 
-    async update(req: Request, res: Response){
+    async update(req: Request, res: Response) {
         const { id } = req.params
         const dados = req.body
-        await unidadeService.update(id, dados)
+        dados.cnes = Number(dados.cnes)
+
+        await unidadeService.update(Number(id), dados)
         res.status(200).send()
     }
 
-    async delete(req: Request, res: Response){
+    async delete(req: Request, res: Response) {
         const { id } = req.params
-        await unidadeService.delete(id)
+        await unidadeService.delete(Number(id))
         res.status(200).send()
     }
-
 }
--- backend/src/services/unidade.service.ts
+++ backend/src/services/unidade.service.ts
@@ -5,7 +5,6 @@ const api = "https://apidadosabertos.saude.gov.br/assistencia-a-saude/unidade-ba
 export class UnidadeService {
     async create(dados: { cnes: number }) {
         const novaUnidade = await fetch(api + `?nu_cnes=${dados.cnes}`)
-        
         const resultado = await novaUnidade.json()
 
         const unidadeEncontrada = resultado.content.find(
@@ -21,5 +20,44 @@ export class UnidadeService {
                 endereco: unidadeEncontrada.nu_endereco || "Endereço não informado",
             }
         })    
+    }
+
+    async findAll() {
+        const unidadesItajuba = await fetch(api + `?co_municipio=313240`)
+        const resultado = await unidadesItajuba.json()
+        return resultado.content
+    }
+
+    async findById(id: number) {
+        const unidade = await prisma.unidadeSaude.findUnique({
+            where: { id }
+        })
+        return unidade
+    }
+
+    async delete(id: number) {
+        await prisma.unidadeSaude.delete({
+            where: { id }
+        })
+    }
+
+    async update(id: number, dados: { cnes: number }) {
+        const novaUnidade = await fetch(api + `?nu_cnes=${dados.cnes}`)
+        const resultado = await novaUnidade.json()
+
+        const unidadeEncontrada = resultado.content.find(
+            (item: any) => Number(item.nu_cnes) === dados.cnes
+        )
+
+        await prisma.unidadeSaude.update({
+            where: { id },
+            data: {
+                cnes: dados.cnes,
+                nome: unidadeEncontrada.no_fantasia,
+                tipo: unidadeEncontrada.ds_tipo_unidade,
+                turno: unidadeEncontrada.ds_turno_atendimento,
+                endereco: unidadeEncontrada.nu_endereco || "Endereço não informado",
+            }
+        })
     }
 }
