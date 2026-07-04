0d9893f8a0f035dd4ad1a7461312c088c18630a1
Thayane Araujo de Castro <thayanearaujo45@gmail.com>
2026-07-04 11:37:18 -0300

    feat: função create criada dentro do service

--- backend/src/services/unidade.service.ts
+++ backend/src/services/unidade.service.ts
@@ -2,6 +2,24 @@ import prisma from "../prisma/client"
 
 const api = "https://apidadosabertos.saude.gov.br/assistencia-a-saude/unidade-basicas-de-saude"
 
-export class UnidadeService{
+export class UnidadeService {
+    async create(dados: { cnes: number }) {
+        const novaUnidade = await fetch(api + `?nu_cnes=${dados.cnes}`)
+        
+        const resultado = await novaUnidade.json()
 
+        const unidadeEncontrada = resultado.content.find(
+            (item: any) => Number(item.nu_cnes) === dados.cnes
+        )
+
+        await prisma.unidadeSaude.create({
+            data: {
+                cnes: dados.cnes,
+                nome: unidadeEncontrada.no_fantasia,
+                tipo: unidadeEncontrada.ds_tipo_unidade,
+                turno: unidadeEncontrada.ds_turno_atendimento,
+                endereco: unidadeEncontrada.nu_endereco || "Endereço não informado",
+            }
+        })    
+    }
 }
